6f95938e41cbf6eaea76022aad9521042cb8b4b1
Fadlyyyy <fadlyynasah86@f=gmail.com>
2022-11-21 18:56:43 +0700

    berantakan slidingnya

--- AnimasiGambar.js
+++ AnimasiGambar.js
@@ -1,22 +1,17 @@
 var x = 0;
 var y = 0;
 
-setInterval(function(){
-
-  x -= 119
-  if (x == -364){
+setInterval(function () {
+  x -= 320;
+  if (x == -920) {
     x = 0;
-    y -= 120;
-
+    y -= 180;
   } else {
-
-    if(y == -362)
-    {
+    if (y == -360) {
       y = 0;
     }
   }
 
-  document.getElementById('stiker').style.backgroundPositionX = String(x) + "px";
-  document.getElementById('stiker').style.backgroundPositionY = String(y) + "px";
-
-  }, 3000 );
+  document.getElementById('stiker').style.backgroundPositionX = String(x) + 'px';
+  document.getElementById('stiker').style.backgroundPositionY = String(x) + 'px';
+}, 1000);
